6824c239d41c018f36a534ba178dfdf313734571
Jiar <78206809+akosighost@users.noreply.github.com>
2026-06-26 20:30:12 +0800

    Setup production database

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "backend link";
+    : "https://reav-on-api.onrender.com";
 
 console.log("Current API URL:", API_BASE_URL);
 
